fe528b8033f2cdd122df865f1a07fa83ab7a6cb1
Thien Thanh <nguyenthanhthien0611@gmail.com>
2022-05-31 23:00:36 +0700

    modified daft_crawl_data.py and adding free_piano_tutorials_dataset.csv file

--- daft_crawl_data.py
+++ daft_crawl_data.py
@@ -20,34 +20,42 @@ list_video_urls = ( pytube.contrib
                           .url_generator() )
 
 video_info = {}
+video_info['title'] = []
+video_info['publish_date'] = []
+video_info['length'] = []
+video_info['views'] = []
 video_info['url'] = []
 
-
-i=1
 while True:
     try:
         video_url = next(list_video_urls)
-        video_info['url'].append(video_url)
         video = YouTube(video_url)
 
-        if video.length < 600:   # Only takes audio smaller 10 minutes.
+        video_info['title'].append(video.title)
+        video_info['publish_date'].append(video.publish_date)
+        video_info['length'].append(video.length)
+        video_info['views'].append(video.views)
+        video_info['url'].append(video_url)
+
+        # Only takes audio smaller 10 minutes and has audio stream !!! 
+        if video.length < 600 and len(video.streams.filter(only_audio=True)) > 0:   
 
             # Filter audio stream only!
             id_tag = video.streams.filter(only_audio=True)[0]         # use the stream has lowest bitrate !!!
-            down_stream = video.streams.get_by_itag(id_tag.itag)   
+         
+            down_stream = video.streams.get_by_itag(id_tag.itag)    # get a specific stream.
 
             down_audio_title = video.title.replace('/','')       # Remove '/' character in audio title.
 
-            out_file_name = "".join(down_audio_title+".wav")
-            down_stream.download( output_path="./dataset",filename=out_file_name)
-            i +=1
+            output_file_name = down_audio_title + ".wav"
+            down_stream.download( output_path="./free_piano_tutorials_dataset",filename=output_file_name)
     except StopIteration:
         break
 
-print(f"Total time: {start_time - time.time()}")
+print(f"Total time: {time.time() - start_time }")
 
 # Export to csv file
-# pd.DataFrame.from_dict(video_info).to_csv(r'test.csv', index=False, header=True)
+pd.DataFrame.from_dict(video_info).to_csv('free_piano_tutorials_dataset_info.csv', index=False, header=True)
 
 
 
